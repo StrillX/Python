5ca357210c634cca27530fbeefc0909b2d7c41ce
StrillX <brunofjm@gmail.com>
2021-03-23 23:25:09 +0000

    travessia tamanho e caminho

--- ficha2.py
+++ ficha2.py
@@ -210,8 +210,248 @@ def saltos(o,d):
 
 print("Saltos")
 print(saltos( (0,0) , (7,7) ))
+rotas = [
+                    ["Porto",20,"Lisboa"],
+                    ["Braga",3,"Barcelos",4,"Viana",3,"Caminha"],
+                    ["Braga",3,"Famalicao",3,"Porto"],
+                    ["Viana",4,"Povoa",3,"Porto"],
+                    ["Lisboa",10,"Evora",8,"Beja",8,"Faro"]
+                    ]
 
-
-def viagem (rotas,o,d):
-
+# @carricosauro
+def fixRotas(rotas):
+    lista = []
     
+    for rota in rotas:
+        posOrigem = 0
+        posDestino = 2
+        posPreco = 1
+        while posDestino + 1 <= len(rota):
+            lista.append([rota[posOrigem],rota[posPreco],rota[posDestino]])
+            posOrigem = posDestino
+            posDestino += 2
+            posPreco += 2
+    
+    return lista
+print(fixRotas(rotas))
+
+
+def viagem(rotas,o,d):
+    rotas = fixRotas(rotas)
+
+    caminhos = {o:0} # { destino:preco }
+    orla = [(o,0)] # [ (destino, preco) ]
+    
+    for cid, preco in orla:
+        proximos = filter(lambda x: cid in x, rotas)
+        
+        
+        for prox in proximos:
+            print(orla)
+            print(proximos)
+            if prox[0] == cid:
+                proximoDestino = prox[2]
+            else:
+                proximoDestino = prox[0]
+            novoPreco = preco + prox[1]
+                
+            if proximoDestino not in caminhos:
+                caminhos[proximoDestino] = novoPreco
+                orla.append((proximoDestino,novoPreco))
+            elif novoPreco < caminhos[proximoDestino]:
+                caminhos[proximoDestino] = novoPreco
+                orla.append((proximoDestino,novoPreco))
+
+    return caminhos[d]
+                
+print("Viagem")
+print(viagem(rotas,"Braga","Caminha"))
+
+
+mapalab = [         "  ########",
+                    "# # #    #",
+                    "# # #### #",
+                    "# #      #",
+                    "# # # ####",
+                    "# # #    #",
+                    "#   # #  #",
+                    "##### ####",
+                    "#        #",
+                    "########  "]
+def adjc(coord,n,mapa):
+    orlacontent = []
+
+
+   
+        #Verificamos eixo do x
+    if coord[0] < n-1 and mapa[coord[1]][coord[0]+1] == ' ' and (coord[2] == "" or coord[2][-1] != 'O'):
+        orlacontent.append((coord[0]+1,coord[1],coord[2]+"E"))
+
+    if coord[0] > 0 and mapa[coord[1]][coord[0]-1] == ' ' and (coord[2] == "" or coord[2][-1] != 'E'):
+        orlacontent.append((coord[0]-1,coord[1],coord[2]+"O"))
+        #Verificamos eixo do y
+    if coord[1] < n-1  and mapa[coord[1]+1][coord[0]] == ' ' and (coord[2] == "" or coord[2][-1] != 'N'):
+        orlacontent.append((coord[0],coord[1]+1,coord[2]+"S"))
+
+    if coord[1] > 0 and mapa[coord[1]-1][coord[0]] == ' ' and (coord[2] == "" or coord[2][-1] != 'S'):
+        orlacontent.append((coord[0],coord[1]-1,coord[2]+"N"))
+
+    return orlacontent
+
+#AKA labirinto
+def caminho (mapa):
+    #Vai guardar as posicoes e movimento feito para chegar la
+    orla = [(0,0,"")]
+    n=len(mapa)
+
+    for coord in orla:
+        if coord[0] == n-1 and coord [1] == n-1:
+            return coord[2]
+    
+        orla += adjc(coord,n,mapa)
+   
+
+
+
+print(caminho(mapalab))
+
+
+
+
+
+'''
+
+Podemos usar um (multi) grafo para representar um mapa de uma cidade: 
+cada nó representa um cruzamento e cada aresta uma rua.
+Pretende-se que implemente uma função que calcula o tamanho de uma cidade, 
+sendo esse tamanho a distância entre os seus cruzamentos mais afastados.
+A entrada consistirá numa lista de nomes de ruas (podendo assumir que os 
+nomes de ruas são únicos). Os identificadores dos cruzamentos correspondem a 
+letras do alfabeto, e cada rua começa (e acaba) no cruzamento 
+identificado pelo primeiro (e último) caracter do respectivo nome.
+
+'''
+
+ruas = ["raio","central","liberdade","chaos","saovictor","saovicente","saodomingos","souto","capelistas","anjo","taxa"]
+
+
+#Algoritmos de Floyd-Warshall que nos foi fornecido
+def fw(adj):
+    dist = {}
+    for o in adj:
+        dist[o] = {}
+        for d in adj:
+            if o == d:
+                dist[o][d] = 0
+            elif d in adj[o]:
+                dist[o][d] = adj[o][d]
+            else:
+                dist[o][d] = float("inf")
+    for k in adj:
+        for o in adj:
+            for d in adj:
+                if dist[o][k] + dist[k][d] < dist[o][d]:
+                    dist[o][d] = dist[o][k] + dist[k][d]
+    return dist
+
+#Vamos definir uma funçao que passe os os argumentos ao algoritmos de Floyd-Warshall
+def buildRuas(ruas):
+    adj= {}
+    for rua in ruas:
+        inicio  =   rua[0]
+        fim     =   rua[-1]
+        comprimento = len(rua)
+
+
+        if inicio not in adj:
+            adj[inicio] = {}
+
+        if fim not in adj:
+            adj[fim] = {}
+
+        if fim not in adj[inicio]:
+            adj[inicio][fim] = float("inf")
+
+
+        if inicio not in adj[fim]:
+            adj[fim][incio] =  float("inf")
+
+        adj[inicio][fim] = min(comprimento,adj[inicio][fim])
+        adj[fim][inicio] = min(comprimento,adj[fim][inicio])
+
+    return adj
+
+def tamanho (ruas):
+    ruas = buildRuas(ruas)
+    distancias = fw(ruas)
+
+    tam = -1
+
+    for dist in distancias:
+        for n in distancias[dist]:
+            m = distancias[dist][n]
+            if m > tam:
+                tam = m
+    return tam
+
+'''
+
+Implemente uma função que calcula o menor custo de atravessar uma região de
+Norte para Sul. O mapa da região é rectangular, dado por uma lista de strings,
+onde cada digito representa a altura de cada ponto. Só é possível efectuar 
+movimentos na horizontal ou na vertical, e só é possível passar de um ponto
+para outro se a diferença de alturas for inferior ou igual a 2, sendo o custo 
+desse movimento 1 mais a diferença de alturas. O ponto de partida (na linha
+mais a Norte) e o ponto de chegada (na linha mais a Sul) não estão fixados à
+partida, devendo a função devolver a coordenada horizontal do melhor
+ponto para iniciar a travessia e o respectivo custo. No caso de haver dois pontos
+com igual custo, deve devolver a coordenada mais a Oeste.
+
+'''
+
+def movsTravessia(mapa,y,x,lenMapa,widthMapa,custo,inicio):
+    lista=[]
+
+    for i in [-1,1]:
+        #Eixo y
+        if 0 <= y+i < lenMapa and y+i != 0:     #Garantimos que estamos dentro do mapa e que nao repetimmos coordenadas
+            delta = abs(int(mapa[y][x]) - int(mapa[y+i][x]))
+            if delta <= 2:
+                lista.append((y+i,x,custo+delta+1,inicio)) 
+
+        #Eixo x
+        if 0 <= x+i < widthMapa and x+i != 0:   #Garantimos que estamos dentro do mapa e que nao repetimmos coordenadas
+            delta = abs(int(mapa[y][x]) - int(mapa[y][x+i]))
+            if delta <= 2:
+                lista.append((y,x+i,custo+delta+1,inicio)) 
+
+    return lista
+
+
+
+def travessia(mapa):
+    lenMapa = len(mapa)
+    widthMapa = len(mapa[0])
+    seen = {}
+    #Orla terá a seguint forma orla=[y,x,custo,inicio] inicio será igual a x no inicio no entanto durante o inicio terá valor fixo e o x variará
+    orla=[]
+    for x in range(widthMapa):
+        orla.append((0,x,0,x))
+        seen[x] = set()
+    menor=(0,float("inf"))
+
+
+    for y, x , custo, inicio in orla:
+        if (x,y) in seen[inicio]:
+            continue
+        seen[inicio].add((x,y))
+        if y == lenMapa-1 and custo < menor[1]:
+            menor = (inicio,custo)
+        orla += movsTravessia(mapa, y , x , lenMapa, widthMapa , custo , inicio)
+
+
+    return menor
+
+
+
+
